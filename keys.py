0a93a90605ac9a49e6aa64fbeaaa2e578115534a
Gustavo Alejandro Cornejo <gcornejo85@gmail.com>
2015-10-12 18:32:24 -0700

    Created Keys.py file
    Obviously actual values are not shown here

---
+++ keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#keys.py
+
+keys = dict(
+ consumer_key =          'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+ consumer_secret =       'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+ access_token =          'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+ access_token_secret =   'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+)
