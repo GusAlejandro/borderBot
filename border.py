6606004490ce3109d5585e74239f9ae0a1e203cd
Gustavo <gcornejo85@gmail.com>
2015-10-23 17:17:38 -0700

    Added a few comments

--- border.py
+++ border.py
@@ -14,6 +14,7 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
+#everything above is just configs and imports
 
 #both functions below are for editing the string to just be the wait time
 def splitted(ztring):
