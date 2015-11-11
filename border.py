54792a0be3a9b8c4bf41e1bb38a70d74fc18aea8
Gustavo <gcornejo85@gmail.com>
2015-11-11 06:07:08 +0000

    Refactored code with OOP mindset

--- border.py
+++ border.py
@@ -1,8 +1,6 @@
 import requests
+import tweepy
 from bs4 import BeautifulSoup
-import tweepy 
-import _json
-import json 
 from keys import keys
 
 CONSUMER_KEY = keys['consumer_key']
@@ -14,66 +12,55 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-#everything above is just configs and imports
 
-#both functions below are for editing the string to just be the wait time
-def splitted(ztring):
-    begin = ztring.index('<span')
-    end = ztring.index('</span>')
-    newString = ztring[begin:end]
-    return newString
 
-def reSplit(fString):
-    begin = fString.index('>')
-    newString = fString[begin:]
-    newString = newString[1:]
-    return newString
+class WaitTime:
+    def __init__(self, url):
+        self.url = url
 
-# Web Scrapes the wait time as raw string 
-def getWait(url):
-    r = requests.get(url)
-    data = r.text
-    soup = BeautifulSoup(data,"html5lib")
-    links = soup.find_all('td',class_="td_col_2 midx")
-    newS = str((links[0])) 
-    return newS
+#scrapes the indicated url and retrieves raw html    
+    def webScrape(self):
+        r = requests.get(self.url)
+        data = r.text
+        soup = BeautifulSoup(data,"html5lib")
+        links = soup.find_all('td',class_="td_col_2")
+        rawScrape = str((links[0]))
+        return rawScrape
 
-#gets wait time as string 
-def carSentri():
-    site = "http://traffic.calit2.net/border/border-wait-times.php?type=passenger&sub=nexus&port=250401"
-    scrape = getWait(site)
-    time = reSplit(splitted(scrape))
-    return time
+#parses html to extract the wait time as a string
+    def getTime(self):
+        rawS = self.webScrape()
+        begin = rawS.index('<span')
+        end = rawS.index('</span>')
+        newString = rawS[begin:end]
+        begin2 = newString.index('>')
+        finalString = newString[begin2:]
+        finalString = finalString[1:]
+        return finalString
 
-def carReady():
-    site = "http://traffic.calit2.net/border/border-wait-times.php?type=passenger&sub=ready&port=250401"
-    scrape =  getWait(site)
-    time = reSplit(splitted(scrape))
-    return time
-    
-def carReg():
-    site = "http://traffic.calit2.net/border/border-wait-times.php?type=passenger&sub=standard&port=250401"
-    scrape = getWait(site)
-    time = reSplit(splitted(scrape))
-    return time
+#creates all the objects needed with their corresponding url 
+carSentri = WaitTime("http://traffic.calit2.net/border/border-wait-times.php?type=passenger&sub=nexus&port=250401")
+carReady = WaitTime("http://traffic.calit2.net/border/border-wait-times.php?type=passenger&sub=ready&port=250401")
+carReg = WaitTime("http://traffic.calit2.net/border/border-wait-times.php?type=passenger&sub=standard&port=250401")
 
-def walkReady():
-    site = "http://traffic.calit2.net/border/border-wait-times.php?type=pedestrian&sub=ready&port=250401"
-    scrape = getWait(site)
-    time = reSplit(splitted(scrape))
-    return time
-    
-def walkReg():
-    site = "http://traffic.calit2.net/border/border-wait-times.php?type=pedestrian&sub=standard&port=250401"
-    scrape = getWait(site)
-    time = reSplit(splitted(scrape))
-    return time
+walkReady = WaitTime("http://traffic.calit2.net/border/border-wait-times.php?type=pedestrian&sub=ready&port=250401")
+walkReg = WaitTime("http://traffic.calit2.net/border/border-wait-times.php?type=pedestrian&sub=standard&port=250401")
 
-#Re-write functions above so it is just one function and a series of variables where the URL's will be stored  
 
 def respondTweet():
-    m = " VIA CAR: Sentri " + carSentri() + ", Ready Lane " + carReady() + ", Regular " + carReg() + " || ON FOOT: Ready Lane " + walkReady()+ ", Regular " + walkReg() 
-    api.update_status(status=m)
-    
+    m = " VIA CAR: Sentri " + carSentri.getTime() + ", Ready Lane " + carReady.getTime() + ", Regular " + carReg.getTime() + " || ON FOOT: Ready Lane " + walkReady.getTime() + ", Regular " + walkReg.getTime() 
+    api.update_status(status=m) 
 
 respondTweet()
+    
+        
+    
+
+
+
+
+
+    
+            
+        
+        
